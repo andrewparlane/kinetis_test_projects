80bdc7107da3f373b0447ddf3eb125aa299fc5b7
Andrew Parlane <andrew.parlane@gmail.com>
2016-08-11 18:34:40 -0400

    LWIP_TEST: Got rid of the sample code from main.c

--- frdm_k66f_board/lwip_test/main.c
+++ frdm_k66f_board/lwip_test/main.c
@@ -28,55 +28,18 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/**
- * This is template for main module created by New Kinetis SDK 2.x Project Wizard. Enjoy!
- **/
-
 #include "board.h"
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "fsl_debug_console.h"
 
-/* FreeRTOS kernel includes. */
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
-#include "timers.h"
-
-
-/* Task priorities. */
-#define hello_task_PRIORITY (configMAX_PRIORITIES - 1)
-
-/*!
- * @brief Task responsible for printing of "Hello world." message.
- */
-static void hello_task(void *pvParameters)
-{
-    for (;;)
-    {
-        PRINTF("Hello world.\r\n");
-        vTaskSuspend(NULL);
-    }
-}
-
-/*!
- * @brief Application entry point.
- */
 int main(void)
 {
-    /* Init board hardware. */
+    // Init pins, clocks and default console
     BOARD_InitPins();
     BOARD_BootClockRUN();
     BOARD_InitDebugConsole();
 
-    /* Create RTOS task */
-    xTaskCreate(hello_task, "Hello_task", configMINIMAL_STACK_SIZE, NULL, hello_task_PRIORITY, NULL);
-    vTaskStartScheduler();
-
-    /* Add your code here */
-
-    for(;;)
-    {   /* Infinite loop to avoid leaving the main function */
-        __asm("NOP"); /* something to use as a breakpoint stop while looping */
-    }
+    // don't exit main()
+    for(;;);
 }
