55045632e5bc523272d504471099e2928ecb9e49
Andrew Parlane <andrew.parlane@gmail.com>
2016-09-05 18:44:06 -0400

    screen_test: Updated FT81X_LIB config file based on the new sample.

--- frdm_k66f_board/screen_test/ft81x_config.h
+++ frdm_k66f_board/screen_test/ft81x_config.h
@@ -14,7 +14,7 @@
 // ----------------------------------------------------------------------------
 // GPU parameters
 // ----------------------------------------------------------------------------
-#define FT81X_GPU_TYPE                          FT81X_GPU_TYPE_FT810
+#define FT81X_GPU_TYPE                          (FT81X_GPU_TYPE_FT810)
 // for resistive touch displays only, determine by experimentation
 #define FT81X_RESISTIVE_TOUCH_THRESHOLD         1800
 
@@ -29,22 +29,29 @@
 // ----------------------------------------------------------------------------
 #define FT81X_SCREEN_WIDTH                      320     // in pixels
 #define FT81X_SCREEN_HEIGHT                     480     // in pixels
-#define FT81X_SCREEN_HORIZ_FRONT_PORCH          0       // REG_HSYNC0 (number of pixel cycles after image data and before hsync pulse)
+#define FT81X_SCREEN_HORIZ_FRONT_PORCH          3       // REG_HSYNC0 (number of pixel cycles after image data and before hsync pulse)
 #define FT81X_SCREEN_HSYNC                      10      // REG_HSYNC1 = this + FT81X_SCREEN_HORIZ_FRONT_PORCH (number of pixel cycles the hsync signal is low)
 #define FT81X_SCREEN_HORIZ_BACK_PORCH           30      // REG_HOFFSET = this + FT81X_SCREEN_HSYNC (number of pixel cycles after the hsync pulse and before image data)
-#define FT81X_SCREEN_VERT_FRONT_PORCH           0       // REG_VSYNC0 (number of lines after image data and before vsync pulse)
+#define FT81X_SCREEN_VERT_FRONT_PORCH           2       // REG_VSYNC0 (number of lines after image data and before vsync pulse)
 #define FT81X_SCREEN_VSYNC                      5       // REG_VSYNC1 = this + FT81X_SCREEN_VERT_FRONT_PORCH (number of lines the vsync signal is low)
 #define FT81X_SCREEN_VERT_BACK_PORCH            5       // REG_VOFFSET = this + FT81X_SCREEN_VSYNC (number of lines after the hsync pulse and before image data)
 
 #define FT81X_SCREEN_PCLK_POL                   1       // 0 = output data on rising edge, 1 = output data on falling edge
-#define FT81X_SCREEN_SWIZZLE                    2       // change what colour / bit is outputed on each pin. See the datasheet Table 4-12 for meanings
+#define FT81X_SCREEN_SWIZZLE                    (FT81X_COLOUR_FORMAT_RGB) // change what colour / bit is outputed on each pin.
 #define FT81X_SCREEN_PCLK_DIVISOR               5       // frame rate = (internalClk (default 60MHz) / this) / (TOTAL_WIDTH * TOTAL_HEIGHT)
 
 // ----------------------------------------------------------------------------
 // Auto calculated parameters, do not edit
 // ----------------------------------------------------------------------------
-#define FT81X_RESISTIVE_TOUCH                   (((FT81X_GPU_TYPE) == FT81X_GPU_TYPE_FT810) || \
-                                                 ((FT81X_GPU_TYPE) == FT81X_GPU_TYPE_FT812))
+#define FT81X_RESISTIVE_TOUCH                   (((FT81X_GPU_TYPE) == (FT81X_GPU_TYPE_FT810)) || \
+                                                 ((FT81X_GPU_TYPE) == (FT81X_GPU_TYPE_FT812)))
+
+#if ((FT81X_GPU_TYPE) == (FT81X_GPU_TYPE_FT810)) || \
+    ((FT81X_GPU_TYPE) == (FT81X_GPU_TYPE_FT811))
+#define FT81X_BITS_PER_PIXEL                    18
+#else
+#define FT81X_BITS_PER_PIXEL                    24
+#endif
 
 #define FT81X_SCREEN_HORIZ_NON_VISIBLE          ((FT81X_SCREEN_HORIZ_FRONT_PORCH) + \
                                                  (FT81X_SCREEN_HSYNC) + \
@@ -60,8 +67,8 @@
 #define FT81X_SCREEN_TOTAL_HEIGHT               ((FT81X_SCREEN_HEIGHT) + \
                                                  (FT81X_SCREEN_VERT_NON_VISIBLE))
 
-#define FT81X_SCREEN_TOTAL_PIXELS               ((FT81X_TOTAL_WIDTH) * \
-                                                 (FT81X_TOTAL_HEIGHT))
+#define FT81X_SCREEN_TOTAL_PIXELS               ((FT81X_SCREEN_TOTAL_WIDTH) * \
+                                                 (FT81X_SCREEN_TOTAL_HEIGHT))
 
 #define FT81X_SCREEN_FRAME_RATE                 ((60000000 / (FT81X_SCREEN_PCLK_DIVISOR)) / (FT81X_SCREEN_TOTAL_PIXELS))
 
