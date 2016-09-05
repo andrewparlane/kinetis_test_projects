9ffe13b72dd2cc08aca2f95515c8f2d85ac8f52e
Andrew Parlane <andrew.parlane@gmail.com>
2016-09-05 18:39:58 -0400

    FT81X_LIB DISPLAY ILI9488: Configuring the display driver based on config options, rather than hard coded values.

--- common/libs/ft81x_lib/include/ft81x/displays/ili9488.h
+++ common/libs/ft81x_lib/include/ft81x/displays/ili9488.h
@@ -1,12 +1,38 @@
 #ifndef __ILI9488_H
 #define __ILI9488_H
 
+#include "ft81x_config.h"
 #include "ft81x/displays/display_types.h"
 
+// ----------------------------------------------------------------------------
 // what comms type this display uses
+// ----------------------------------------------------------------------------
 #define FT81X_DISPLAY_COMMS_TYPE        FT81X_DISPLAY_COMMS_TYPE_SPI
 #define FT81X_DISPLAY_SPI_MODE          FT81X_DISPLAY_SPI_MODE_0
 #define FT81X_DISPLAY_SPI_BIT_ORDER     FT81X_DISPLAY_SPI_BIT_ORDER_MSB
 #define FT81X_DISPLAY_SPI_CS_POLARITY   FT81X_DISPLAY_SPI_CS_ACTIVE_LOW
 
+// ----------------------------------------------------------------------------
+// check that the ILI9488 supports the config.
+// ----------------------------------------------------------------------------
+#if ((FT81X_SCREEN_SWIZZLE) != (FT81X_COLOUR_FORMAT_RGB)) && \
+    ((FT81X_SCREEN_SWIZZLE) != (FT81X_COLOUR_FORMAT_BGR))
+#  error Unsuported pinout
+#endif
+
+#if ((FT81X_SCREEN_HORIZ_BACK_PORCH) < 3) || \
+    ((FT81X_SCREEN_HORIZ_BACK_PORCH) > 256) || \
+    ((FT81X_SCREEN_HSYNC) < 3) || \
+    ((FT81X_SCREEN_HORIZ_FRONT_PORCH) < 3) || \
+    ((FT81X_SCREEN_HORIZ_FRONT_PORCH) > 256)
+#error Horizontal blanking settings unsupported
+#endif
+
+#if ((FT81X_SCREEN_VERT_BACK_PORCH) < 2) || \
+    ((FT81X_SCREEN_VERT_BACK_PORCH) > 31) || \
+    ((FT81X_SCREEN_VSYNC) < 1) || \
+    ((FT81X_SCREEN_VERT_FRONT_PORCH) < 2) || \
+    ((FT81X_SCREEN_VERT_FRONT_PORCH) > 31)
+#error Vertical blanking settings unsupported
+#endif
 #endif
--- common/libs/ft81x_lib/src/displays/ili9488.c
+++ common/libs/ft81x_lib/src/displays/ili9488.c
@@ -17,6 +17,7 @@
 #define CMD_PAGE_ADDR_SET               0x2B
 #define CMD_MEMORY_WRITE                0x2C
 #define CMD_MEM_ACCESS_CTRL             0x36
+#define     MEM_ACCESS_CTRL_BGR                 0x08
 #define CMD_INTERFACE_PIXEL_FORMAT      0x3A
 #define     INTERFACE_PIXEL_FORMAT_18BIT_DPI    0x60
 #define     INTERFACE_PIXEL_FORMAT_18BIT_DBI    0x06
@@ -24,7 +25,12 @@
 
 // extended commands
 #define CMD_INTERFACE_MODE_CONTROL      0xB0
+#define     INTERFACE_MODE_CONTROL_DPL(dpl)             (((dpl) & 0x01) << 1)
 #define CMD_FRAME_RATE_CONTROL          0xB1
+#define     FRAME_RATE_CONTROL_PARAM_0_FRS(frs)         (((frs) & 0x0F) << 4)
+#define     FRAME_RATE_CONTROL_PARAM_0_DIVA_DEFAULT     0
+#define     FRAME_RATE_CONTROL_PARAM_1_RTNA_DEFAULT     0x11
+#define CMD_BLANKING_PORCH_CONTROL      0xB5
 #define CMD_DISPLAY_FUNCTION_CONTROL    0xB6
 #define CMD_SET_IMAGE_FUNCTION          0xE9
 #define CMD_ADJUST_CONTROL_3            0xF7
@@ -168,10 +174,30 @@ ft81x_result ft81x_display_send_configuration(void *platform_user_data)
     data[3] = ((FT81X_SCREEN_HEIGHT)-1) & 0xFF;
     WRITE_CMD(CMD_PAGE_ADDR_SET, 4, data);
 
+    // configure the blanking settings
+    data[0] = (FT81X_SCREEN_VERT_FRONT_PORCH);
+    data[1] = (FT81X_SCREEN_VERT_BACK_PORCH);
+    data[2] = (FT81X_SCREEN_HORIZ_FRONT_PORCH);
+    data[3] = (FT81X_SCREEN_HORIZ_BACK_PORCH);
+    WRITE_CMD(CMD_BLANKING_PORCH_CONTROL, 4, data);
+
     // configure frame rate
-# warning TODO configure based on defines
-    data[0] = 0xB0;
-    WRITE_CMD(CMD_FRAME_RATE_CONTROL, 1, data);
+    // this calulation is kind of confusing, the data sheet is not very clear
+    // so we simplify things a bit.
+    //   Fosc = 18MHz (I think this is always the case)
+    //   DIVA = 0. IE. use Fosc as the clock, don't divide it
+    //   RTNA = 0x11 (17) the default
+    // using these we get the equation
+    //   frame rate = Fosc / (2**DIVA * (76 - (FRS*4)) * RTNA * total_height)
+    // we can rearrange this for FRS
+    //   FRS = 19 - (Fosc / (frame rate * 2**DIVA * RTNA * total_height * 4))
+    const uint32_t fosc = 18*1000*1000;
+    const uint8_t diva = (FRAME_RATE_CONTROL_PARAM_0_DIVA_DEFAULT);
+    const uint8_t rtna = (FRAME_RATE_CONTROL_PARAM_1_RTNA_DEFAULT);
+    const uint8_t frs = 19 - (fosc / ((FT81X_SCREEN_FRAME_RATE) * (1 << diva) * rtna * FT81X_SCREEN_TOTAL_HEIGHT * 4));
+    data[0] = (FRAME_RATE_CONTROL_PARAM_0_FRS(frs)) | diva;
+    data[1] = rtna;
+    WRITE_CMD(CMD_FRAME_RATE_CONTROL, 2, data);
 
     // DSI with DCS command, use loose packet ???
     data[0] = ADJUST_CONTROL_PARAM_0;
@@ -180,31 +206,40 @@ ft81x_result ft81x_display_send_configuration(void *platform_user_data)
     data[3] = ADJUST_CONTROL_PARAM_3 | ADJUST_CONTROL_PARAM_3_LOOSE_PKT;
     WRITE_CMD(CMD_ADJUST_CONTROL_3, 4, data);
 
-    // Set DOTCLK polarity for falling edge
-    data[0] = 2;
+    // Set DOTCLK polarity to match the GPU.
+    // if the GPU outputs on the falling edge,
+    // then we read on the rising edge,
+    // and vice versa
+    data[0] = INTERFACE_MODE_CONTROL_DPL(FT81X_SCREEN_PCLK_POL);
     WRITE_CMD(CMD_INTERFACE_MODE_CONTROL, 1, data);
 
     // set the pixel format to 18 bit for both RGB and MCU interfaces
+#if ((FT81X_BITS_PER_PIXEL) == 18)
     data[0] = INTERFACE_PIXEL_FORMAT_18BIT_DPI | INTERFACE_PIXEL_FORMAT_18BIT_DBI;
+#else
+    // todo 24 bit - look at CMD_SET_IMAGE_FUNCTION??
+#error Unsupported pixel format
+#endif
     WRITE_CMD(CMD_INTERFACE_PIXEL_FORMAT, 1, data);
-
-    // disable 24 bit data bus (think this is the default)
-    data[0] = 0;
-    WRITE_CMD(CMD_SET_IMAGE_FUNCTION, 1, data);
 
     // enable backlight and dimming control
     data[0] = 0x2C;
     WRITE_CMD(CMD_WRITE_CONTROL_DISPLAY_VAL, 1, data);
 
-    // Set pixel format to BGR
-    // and reflect column data (why?)
-    data[0] = 0x48;
+    // Reflect column data (why?)
+    data[0] = 0x40;
+#if ((FT81X_SCREEN_SWIZZLE) == (FT81X_COLOUR_FORMAT_BGR))
+    // set to BGR (ie blue and red pins swapped)
+    data[0] |= MEM_ACCESS_CTRL_BGR;
+#endif
     WRITE_CMD(CMD_MEM_ACCESS_CTRL, 1, data);
 
-    // not really sure on this
-    data[0] = 0x30;
-    data[1] = 0x02;
-    data[2] = 0x3b;
+    // set some display functions
+    data[0] = 0x30;     // not really sure
+    data[1] = 0x02;     // default
+    // how many visible lines in intervals of 8.
+    // (NL[5:0] + 1) * 8 must be >= number of visible lines (FT81X_SCREEN_HEIGHT)
+    data[2] = ((uint8_t)(((FT81X_SCREEN_HEIGHT) + 7) / 8)) - 1;
     WRITE_CMD(CMD_DISPLAY_FUNCTION_CONTROL, 3, data);
 
     // put the ILI9488 into low power mode (but still working)
