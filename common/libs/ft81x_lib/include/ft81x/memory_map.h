395681462b1011d00c8e015ef36f9f79d8ee8e9e
Andrew Parlane <andrew.parlane@gmail.com>
2016-09-12 17:18:20 -0400

    FT81X_LIB: Added some more registers.

--- common/libs/ft81x_lib/include/ft81x/memory_map.h
+++ common/libs/ft81x_lib/include/ft81x/memory_map.h
@@ -5,6 +5,16 @@
 
 #define FT81X_REG_ID_ADDR           0x302000
 #define     FT81X_REG_ID_EXPECTED           0x7C
+#define FT81X_REG_FRAMES            0x302004
+#define FT81X_REG_CLOCK             0x302008
+#define FT81X_REG_FREQUENCY         0x30200C
+#define FT81X_REG_RENDERMODE        0x302010
+#define FT81X_REG_SNAPY             0x302014
+#define FT81X_REG_SNAPSHOT          0x302018
+#define FT81X_REG_SNAPFORMAT        0x30201C
+#define FT81X_REG_CPURESET          0x302020
+#define FT81X_REG_TAP_CRC           0x302024
+#define FT81X_REG_TAP_MASK          0x302028
 #define FT81X_REG_HCYCLE            0x30202C
 #define FT81X_REG_HOFFSET           0x302030
 #define FT81X_REG_HSIZE             0x302034
@@ -17,6 +27,8 @@
 #define FT81X_REG_VSYNC1            0x302050
 #define FT81X_REG_DLSWAP            0x302054
 #define     FT81X_REG_DLSWAP_SWAP_FRAME     0x02
+#define FT81X_REG_ROTATE            0x302058
+#define FT81X_REG_OUTBITS           0x30205C
 #define FT81X_REG_DITHER            0x302060
 #define FT81X_REG_SWIZZLE           0x302064
 #define FT81X_REG_CSPREAD           0x302068
@@ -25,9 +37,15 @@
 #define FT81X_REG_GPIO_DIR          0x302090
 #define FT81X_REG_GPIO              0x302094
 #define     FT81X_REG_GPIO_DISP             0x80
+#define FT81X_REG_INT_FLAGS         0x3020A8
+#define FT81X_REG_INT_EN            0x3020AC
+#define FT81X_REG_INT_MASK          0x3020B0
 #define FT81X_REG_PWM_DUTY          0x3020D4
 #define     FT81X_REG_PWM_DUTY_OFF          0
 #define     FT81X_REG_PWM_DUTY_MAX          128
+#define FT81X_REG_CMD_READ          0x3020F8
+#define FT81X_REG_CMD_WRITE         0x3020FC
+#define FT81X_REG_CMD_DL            0x302100
 #define FT81X_REG_TOUCH_RZTHRESH    0x302118
 
 #endif
