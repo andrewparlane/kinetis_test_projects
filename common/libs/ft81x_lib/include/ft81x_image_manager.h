992af53d0cf4a5553529ec234423beba8fa75c15
Andrew Parlane <andrew.parlane@gmail.com>
2016-09-22 13:35:45 -0400

    FT81X_LIB Image Manager: Changed the image manager helper macros to functions that use the new ability to send display list snippets.

--- common/libs/ft81x_lib/include/ft81x_image_manager.h
+++ common/libs/ft81x_lib/include/ft81x_image_manager.h
@@ -34,51 +34,6 @@ typedef struct
     uint32_t    lut_load_offset;    // offset of LUT in g_ram (if format is paletted)
 } FT81X_Image_Handle;
 
-// ----------------------------------------------------------------------------
-// Helper macros
-// ----------------------------------------------------------------------------
-#define FT81X_IMAGE_MANAGER_DL_INIT_IMAGE(image_properties, image_handle, filter, wrapx, wrapy)                         \
-    FT81X_DL_CMD_BITMAP_HANDLE((image_handle).bitmap_handle),                                                           \
-    FT81X_DL_CMD_BITMAP_LAYOUT((image_properties).format, (image_properties).linestride, (image_properties).height),    \
-    FT81X_DL_CMD_BITMAP_SIZE((filter), (wrapx), (wrapy), (image_properties).width, (image_properties).height),          \
-    FT81X_DL_CMD_BITMAP_SOURCE((image_handle).load_offset)
-
-
-#define FT81X_IMAGE_MANAGER_DL_DRAW_NON_PALETTED_IMAGE(image_handle, x, y)  \
-    FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_BITMAP),                             \
-        FT81X_DL_CMD_VERTEX2II((x), (y), (image_handle).bitmap_handle, 0),  \
-    FT81X_DL_CMD_END()
-
-#define FT81X_IMAGE_MANAGER_DL_DRAW_PALETTED_IMAGE(image_handle, x, y)                              \
-    FT81X_DL_CMD_BEGIN((FT81X_PRIMITIVE_BITMAP)),                                                   \
-        /* save the context so we can restore it afterwards */                                      \
-        FT81X_DL_CMD_SAVE_CONTEXT(),                                                                \
-                                                                                                    \
-        /* alpha */                                                                                 \
-        FT81X_DL_CMD_BLEND_FUNC(FT81X_BLEND_FUNC_ONE, FT81X_BLEND_FUNC_ZERO),                       \
-        FT81X_DL_CMD_COLOUR_MASK(0, 0, 0, 1),                                                       \
-        FT81X_DL_CMD_PALETTE_SOURCE((image_handle).lut_load_offset+3),                              \
-        FT81X_DL_CMD_VERTEX2II((x), (y), (image_handle).bitmap_handle, 0),                          \
-                                                                                                    \
-        /* colours */                                                                               \
-        FT81X_DL_CMD_BLEND_FUNC(FT81X_BLEND_FUNC_DST_ALPHA, FT81X_BLEND_FUNC_ONE_MINUS_DST_ALPHA),  \
-        /* red */                                                                                   \
-        FT81X_DL_CMD_COLOUR_MASK(1, 0, 0, 0),                                                       \
-        FT81X_DL_CMD_PALETTE_SOURCE((image_handle).lut_load_offset+2),                              \
-        FT81X_DL_CMD_VERTEX2II((x), (y), (image_handle).bitmap_handle, 0),                          \
-        /* green */                                                                                 \
-        FT81X_DL_CMD_COLOUR_MASK(0, 1, 0, 0),                                                       \
-        FT81X_DL_CMD_PALETTE_SOURCE((image_handle).lut_load_offset+1),                              \
-        FT81X_DL_CMD_VERTEX2II((x), (y), (image_handle).bitmap_handle, 0),                          \
-        /* blue */                                                                                  \
-        FT81X_DL_CMD_COLOUR_MASK(0, 0, 1, 0),                                                       \
-        FT81X_DL_CMD_PALETTE_SOURCE((image_handle).lut_load_offset+0),                              \
-        FT81X_DL_CMD_VERTEX2II((x), (y), (image_handle).bitmap_handle, 0),                          \
-                                                                                                    \
-        /* revert the changes to colour mask and blend func */                                      \
-        FT81X_DL_CMD_RESTORE_CONTEXT(),                                                             \
-    FT81X_DL_CMD_END()
-
 // forward declarations for ft81x.h
 enum _ft81x_result;
 typedef enum _ft81x_result ft81x_result;
@@ -89,5 +44,9 @@ typedef struct _FT81X_Handle FT81X_Handle;
 ft81x_result ft81x_image_manager_initialise(FT81X_Handle *handle);
 
 ft81x_result ft81x_image_manager_load_image(FT81X_Handle *handle, const FT81X_Image_Properties *image_properties, FT81X_Image_Handle *image_handle);
+
+ft81x_result ft81x_image_manager_send_image_init_dl(FT81X_Handle *handle, const FT81X_Image_Properties *image_properties, const FT81X_Image_Handle *image_handle, FT81X_Bitmap_Filter filter, FT81X_Bitmap_Wrap wrapx, FT81X_Bitmap_Wrap wrapy);
+ft81x_result ft81x_image_manager_send_non_paletted8_image_draw_dl(FT81X_Handle *handle, const FT81X_Image_Handle *image_handle, uint32_t x, uint32_t y);
+ft81x_result ft81x_image_manager_send_paletted8_image_draw_dl(FT81X_Handle *handle, const FT81X_Image_Handle *image_handle, uint32_t x, uint32_t y);
 
 #endif
--- common/libs/ft81x_lib/src/image_manager.c
+++ common/libs/ft81x_lib/src/image_manager.c
@@ -88,3 +88,68 @@ ft81x_result ft81x_image_manager_load_image(FT81X_Handle *handle, const FT81X_Im
 
     return FT81X_RESULT_OK;
 }
+
+// ----------------------------------------------------------------------------
+// Display list functions
+// ----------------------------------------------------------------------------
+ft81x_result ft81x_image_manager_send_image_init_dl(FT81X_Handle *handle, const FT81X_Image_Properties *image_properties, const FT81X_Image_Handle *image_handle, FT81X_Bitmap_Filter filter, FT81X_Bitmap_Wrap wrapx, FT81X_Bitmap_Wrap wrapy)
+{
+    const uint32_t dl[] =
+    {
+        FT81X_DL_CMD_BITMAP_HANDLE(image_handle->bitmap_handle),
+        FT81X_DL_CMD_BITMAP_LAYOUT(image_properties->format, image_properties->linestride, image_properties->height),
+        FT81X_DL_CMD_BITMAP_SIZE(filter, wrapx, wrapy, image_properties->width, image_properties->height),
+        FT81X_DL_CMD_BITMAP_SOURCE(image_handle->load_offset)
+    };
+
+    return ft81x_write_display_list_snippet(handle, sizeof(dl), dl);
+}
+
+ft81x_result ft81x_image_manager_send_non_paletted8_image_draw_dl(FT81X_Handle *handle, const FT81X_Image_Handle *image_handle, uint32_t x, uint32_t y)
+{
+    const uint32_t dl[] =
+    {
+        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_BITMAP),
+            FT81X_DL_CMD_VERTEX2II(x, y, image_handle->bitmap_handle, 0),
+        FT81X_DL_CMD_END()
+    };
+
+    return ft81x_write_display_list_snippet(handle, sizeof(dl), dl);
+}
+
+ft81x_result ft81x_image_manager_send_paletted8_image_draw_dl(FT81X_Handle *handle, const FT81X_Image_Handle *image_handle, uint32_t x, uint32_t y)
+{
+    const uint32_t dl[] =
+    {
+        FT81X_DL_CMD_BEGIN((FT81X_PRIMITIVE_BITMAP)),
+            /* save the context so we can restore it afterwards */
+            FT81X_DL_CMD_SAVE_CONTEXT(),
+
+            /* alpha */
+            FT81X_DL_CMD_BLEND_FUNC(FT81X_BLEND_FUNC_ONE, FT81X_BLEND_FUNC_ZERO),
+            FT81X_DL_CMD_COLOUR_MASK(0, 0, 0, 1),
+            FT81X_DL_CMD_PALETTE_SOURCE(image_handle->lut_load_offset+3),
+            FT81X_DL_CMD_VERTEX2II(x, y, image_handle->bitmap_handle, 0),
+
+            /* colours */
+            FT81X_DL_CMD_BLEND_FUNC(FT81X_BLEND_FUNC_DST_ALPHA, FT81X_BLEND_FUNC_ONE_MINUS_DST_ALPHA),
+            /* red */
+            FT81X_DL_CMD_COLOUR_MASK(1, 0, 0, 0),
+            FT81X_DL_CMD_PALETTE_SOURCE(image_handle->lut_load_offset+2),
+            FT81X_DL_CMD_VERTEX2II(x, y, image_handle->bitmap_handle, 0),
+            /* green */
+            FT81X_DL_CMD_COLOUR_MASK(0, 1, 0, 0),
+            FT81X_DL_CMD_PALETTE_SOURCE(image_handle->lut_load_offset+1),
+            FT81X_DL_CMD_VERTEX2II(x, y, image_handle->bitmap_handle, 0),
+            /* blue */
+            FT81X_DL_CMD_COLOUR_MASK(0, 0, 1, 0),
+            FT81X_DL_CMD_PALETTE_SOURCE(image_handle->lut_load_offset+0),
+            FT81X_DL_CMD_VERTEX2II(x, y, image_handle->bitmap_handle, 0),
+
+            /* revert the changes to colour mask and blend func */
+            FT81X_DL_CMD_RESTORE_CONTEXT(),
+        FT81X_DL_CMD_END()
+    };
+
+    return ft81x_write_display_list_snippet(handle, sizeof(dl), dl);
+}
