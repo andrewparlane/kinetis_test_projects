5100005323ac76f29d3eec8f541a46316b06ea7b
Andrew Parlane <andrew.parlane@gmail.com>
2016-09-22 16:16:55 -0400

    screen_test: Moved the current test display stuff to a new file.
    Plan is to start on a new test using the co-proc.

--- frdm_k66f_board/screen_test/main.c
+++ frdm_k66f_board/screen_test/main.c
@@ -46,14 +46,6 @@
 
 // ft81x lib includes
 #include "ft81x.h"
-#include "ft81x_display_list.h"
-#include "ft81x_image_manager.h"
-#include "ft81x_text_manager.h"
-
-// resources
-#include "resources/cat_l8_raw.h"
-#include "resources/cat_argb1555_raw.h"
-#include "resources/cat_paletted8_raw.h"
 
 // ----------------------------------------------------------------------------
 // GPIO pins
@@ -77,97 +69,12 @@ const GPIO_Info *pin_config[] = { &screen_int_pin };
 // ----------------------------------------------------------------------------
 #define DL_BUFFER_SIZE 512
 uint8_t dl_buffer[DL_BUFFER_SIZE];
-const uint32_t clear_dl_snippet[] =
-{
-    // set the background colour to dark grey and clear the screen
-    FT81X_DL_CMD_CLEAR_COLOUR_RGB(32,32,32),
-    FT81X_DL_CMD_CLEAR(1,1,1),
-};
-
-const uint32_t test_dl_snippet[] =
-{
-        // draw some points
-        FT81X_DL_CMD_POINT_SIZE(20 * 16),
-        FT81X_DL_CMD_COLOUR_RGB(255, 0, 0),
-        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_POINTS),
-            FT81X_DL_CMD_VERTEX2II(5, 5, 0, 0),
-        FT81X_DL_CMD_END(),
-
-        FT81X_DL_CMD_POINT_SIZE(40 * 16),
-        FT81X_DL_CMD_COLOUR_RGB(0, 255, 0),
-        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_POINTS),
-            FT81X_DL_CMD_VERTEX2II(315, 5, 0, 0),
-        FT81X_DL_CMD_END(),
-
-        // draw a line
-        FT81X_DL_CMD_LINE_WIDTH(5 * 16),
-        FT81X_DL_CMD_COLOUR_RGB(0, 0, 255),
-        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_LINES),
-            FT81X_DL_CMD_VERTEX2II(10, 10, 0, 0),
-            FT81X_DL_CMD_VERTEX2II(310, 470, 0, 0),
-        FT81X_DL_CMD_END(),
-
-        // draw a rect
-        FT81X_DL_CMD_LINE_WIDTH(1 * 16),
-        FT81X_DL_CMD_COLOUR_RGB(255, 255, 0),
-        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_RECTS),
-            FT81X_DL_CMD_VERTEX2II(300, 199, 0, 0),
-            FT81X_DL_CMD_VERTEX2II(200, 280, 0, 0),
-        FT81X_DL_CMD_END(),
-
-        // draw a rect with rounded corners
-        FT81X_DL_CMD_LINE_WIDTH(10 * 16),
-        FT81X_DL_CMD_COLOUR_RGB(255, 0, 255),
-        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_RECTS),
-            FT81X_DL_CMD_VERTEX2II(50, 300, 0, 0),
-            FT81X_DL_CMD_VERTEX2II(100, 450, 0, 0),
-        FT81X_DL_CMD_END(),
-};
 
 // ----------------------------------------------------------------------------
-// Test display functions
+// Test forward defines
 // ----------------------------------------------------------------------------
-ft81x_result test_text(FT81X_Handle *handle)
-{
-    ft81x_result res;
 
-    // set the font to green
-    res = ft81x_write_display_list_cmd(handle, FT81X_DL_CMD_COLOUR_RGB(0, 255, 0));
-    if (res != FT81X_RESULT_OK)
-    {
-        return res;
-    }
-
-    res = ft81x_text_manager_write_fixed_width_font_string(handle,
-                                                           24,
-                                                           FT81X_TEXT_MANAGER_FONT_24_MAX_WIDTH,
-                                                           FT81X_TEXT_MANAGER_FONT_24_HEIGHT,
-                                                           "How much wood\nwould a wood chuck\nchuck, if a wood\nchuck could\nchuck wood?",
-                                                           10,
-                                                           40);
-    if (res != FT81X_RESULT_OK)
-    {
-        return res;
-    }
-
-    // set the font to cyan
-    res = ft81x_write_display_list_cmd(handle, FT81X_DL_CMD_COLOUR_RGB(0, 255, 255));
-    if (res != FT81X_RESULT_OK)
-    {
-        return res;
-    }
-
-    res = ft81x_text_manager_write_fixed_width_font_string(handle,
-                                                           30,
-                                                           FT81X_TEXT_MANAGER_FONT_30_MAX_WIDTH,
-                                                           FT81X_TEXT_MANAGER_FONT_30_HEIGHT,
-                                                           "Peter\nPiper\npicked...",
-                                                           10,
-                                                           300);
-
-
-    return res;
-}
+ft81x_result test1(FT81X_Handle *handle);
 
 // ----------------------------------------------------------------------------
 // Main thread
@@ -222,170 +129,24 @@ static void main_thread(void *arg)
     }
 
     // ----------------------------------------------------------
-    // load our resources into g RAM
-    // ----------------------------------------------------------
-    // first is the L8 image
-    FT81X_Image_Handle cat_l8_raw_image_handle;
-    res = ft81x_image_manager_load_image(&handle, &cat_l8_raw_image_properties, &cat_l8_raw_image_handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_image_manager_load_image failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // then the argb1555 image
-    FT81X_Image_Handle cat_argb1555_raw_image_handle;
-    res = ft81x_image_manager_load_image(&handle, &cat_argb1555_raw_image_properties, &cat_argb1555_raw_image_handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_image_manager_load_image failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // then the paletted8 image
-    FT81X_Image_Handle cat_paletted8_raw_image_handle;
-    res = ft81x_image_manager_load_image(&handle, &cat_paletted8_raw_image_properties, &cat_paletted8_raw_image_handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_image_manager_load_image failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // ----------------------------------------------------------
-    // Build and display the display list
-    // ----------------------------------------------------------
-    // clear the screen
-    res = ft81x_write_display_list_snippet(&handle, sizeof(clear_dl_snippet), clear_dl_snippet);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // initialise the L8 image in the DL
-    res = ft81x_image_manager_send_image_init_dl(&handle,
-                                                 &cat_l8_raw_image_properties,
-                                                 &cat_l8_raw_image_handle,
-                                                 FT81X_BITMAP_FILTER_NEAREST,
-                                                 FT81X_BITMAP_WRAP_BORDER,
-                                                 FT81X_BITMAP_WRAP_BORDER);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // initialise the argb1555 image in the DL
-    res = ft81x_image_manager_send_image_init_dl(&handle,
-                                                 &cat_argb1555_raw_image_properties,
-                                                 &cat_argb1555_raw_image_handle,
-                                                 FT81X_BITMAP_FILTER_NEAREST,
-                                                 FT81X_BITMAP_WRAP_BORDER,
-                                                 FT81X_BITMAP_WRAP_BORDER);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // initialise the paletted8 image in the DL
-    res = ft81x_image_manager_send_image_init_dl(&handle,
-                                                 &cat_paletted8_raw_image_properties,
-                                                 &cat_paletted8_raw_image_handle,
-                                                 FT81X_BITMAP_FILTER_NEAREST,
-                                                 FT81X_BITMAP_WRAP_BORDER,
-                                                 FT81X_BITMAP_WRAP_BORDER);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // draw L8 image
-    res = ft81x_image_manager_send_non_paletted8_image_draw_dl(&handle,
-                                                               &cat_l8_raw_image_handle,
-                                                               160-(cat_l8_raw_image_properties.width/2),
-                                                               240-(cat_l8_raw_image_properties.height/2));
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // draw ARGB1555 image
-    res = ft81x_image_manager_send_non_paletted8_image_draw_dl(&handle,
-                                                               &cat_argb1555_raw_image_handle,
-                                                               0, 0);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // draw paletted8 image
-    // (note saves the context on the stack and restores it)
-    res = ft81x_image_manager_send_paletted8_image_draw_dl(&handle,
-                                                           &cat_paletted8_raw_image_handle,
-                                                           162, 270);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // draw some points, lines and rects
-    res = ft81x_write_display_list_snippet(&handle, sizeof(test_dl_snippet), test_dl_snippet);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // write some text strings
-    res = test_text(&handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("test_text failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // display it
-    res = ft81x_write_display_list_cmd(&handle, FT81X_DL_CMD_DISPLAY());
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_write_display_list_cmd failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // write everything to the DL ram and then swap it in
-    res = ft81x_end_display_list(&handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_end_display_list failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // ----------------------------------------------------------
     // enable the backlight
+    // you can do this after displaying the first screen
+    // to avoid the black screen)
     // ----------------------------------------------------------
     res = ft81x_backlight(&handle, FT81X_BACKLIGHT_LEVEL_MAX);
     if (res != FT81X_RESULT_OK)
     {
         DbgConsole_Printf("ft81x_backlight failed with %u\n", res);
+        ft81x_cleanup(&handle);
+        return;
+    }
+
+    // draws some images, primitives and text
+    // all directly through the DL
+    res = test1(&handle);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("test1 failed with %u\n", res);
         ft81x_cleanup(&handle);
         return;
     }
---
+++ frdm_k66f_board/screen_test/tests/test1.c
@@ -0,0 +1,252 @@
+#include "fsl_debug_console.h"
+
+// ft81x_lib includes
+#include "ft81x.h"
+#include "ft81x_display_list.h"
+#include "ft81x_image_manager.h"
+#include "ft81x_text_manager.h"
+
+// resources
+#include "resources/cat_l8_raw.h"
+#include "resources/cat_argb1555_raw.h"
+#include "resources/cat_paletted8_raw.h"
+
+static const uint32_t clear_dl_snippet[] =
+{
+    // set the background colour to dark grey and clear the screen
+    FT81X_DL_CMD_CLEAR_COLOUR_RGB(32,32,32),
+    FT81X_DL_CMD_CLEAR(1,1,1),
+};
+
+static const uint32_t test_dl_snippet[] =
+{
+        // draw some points
+        FT81X_DL_CMD_POINT_SIZE(20 * 16),
+        FT81X_DL_CMD_COLOUR_RGB(255, 0, 0),
+        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_POINTS),
+            FT81X_DL_CMD_VERTEX2II(5, 5, 0, 0),
+        FT81X_DL_CMD_END(),
+
+        FT81X_DL_CMD_POINT_SIZE(40 * 16),
+        FT81X_DL_CMD_COLOUR_RGB(0, 255, 0),
+        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_POINTS),
+            FT81X_DL_CMD_VERTEX2II(315, 5, 0, 0),
+        FT81X_DL_CMD_END(),
+
+        // draw a line
+        FT81X_DL_CMD_LINE_WIDTH(5 * 16),
+        FT81X_DL_CMD_COLOUR_RGB(0, 0, 255),
+        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_LINES),
+            FT81X_DL_CMD_VERTEX2II(10, 10, 0, 0),
+            FT81X_DL_CMD_VERTEX2II(310, 470, 0, 0),
+        FT81X_DL_CMD_END(),
+
+        // draw a rect
+        FT81X_DL_CMD_LINE_WIDTH(1 * 16),
+        FT81X_DL_CMD_COLOUR_RGB(255, 255, 0),
+        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_RECTS),
+            FT81X_DL_CMD_VERTEX2II(300, 199, 0, 0),
+            FT81X_DL_CMD_VERTEX2II(200, 280, 0, 0),
+        FT81X_DL_CMD_END(),
+
+        // draw a rect with rounded corners
+        FT81X_DL_CMD_LINE_WIDTH(10 * 16),
+        FT81X_DL_CMD_COLOUR_RGB(255, 0, 255),
+        FT81X_DL_CMD_BEGIN(FT81X_PRIMITIVE_RECTS),
+            FT81X_DL_CMD_VERTEX2II(50, 300, 0, 0),
+            FT81X_DL_CMD_VERTEX2II(100, 450, 0, 0),
+        FT81X_DL_CMD_END(),
+};
+
+static ft81x_result test1_text(FT81X_Handle *handle)
+{
+    ft81x_result res;
+
+    // set the font to green
+    res = ft81x_write_display_list_cmd(handle, FT81X_DL_CMD_COLOUR_RGB(0, 255, 0));
+    if (res != FT81X_RESULT_OK)
+    {
+        return res;
+    }
+
+    res = ft81x_text_manager_write_fixed_width_font_string(handle,
+                                                           24,
+                                                           FT81X_TEXT_MANAGER_FONT_24_MAX_WIDTH,
+                                                           FT81X_TEXT_MANAGER_FONT_24_HEIGHT,
+                                                           "How much wood\nwould a wood chuck\nchuck, if a wood\nchuck could\nchuck wood?",
+                                                           10,
+                                                           40);
+    if (res != FT81X_RESULT_OK)
+    {
+        return res;
+    }
+
+    // set the font to cyan
+    res = ft81x_write_display_list_cmd(handle, FT81X_DL_CMD_COLOUR_RGB(0, 255, 255));
+    if (res != FT81X_RESULT_OK)
+    {
+        return res;
+    }
+
+    res = ft81x_text_manager_write_fixed_width_font_string(handle,
+                                                           30,
+                                                           FT81X_TEXT_MANAGER_FONT_30_MAX_WIDTH,
+                                                           FT81X_TEXT_MANAGER_FONT_30_HEIGHT,
+                                                           "Peter\nPiper\npicked...",
+                                                           10,
+                                                           300);
+
+
+    return res;
+}
+
+ft81x_result test1(FT81X_Handle *handle)
+{
+    ft81x_result res;
+
+    // ----------------------------------------------------------
+    // load our resources into g RAM
+    // ----------------------------------------------------------
+    // first is the L8 image
+    FT81X_Image_Handle cat_l8_raw_image_handle;
+    res = ft81x_image_manager_load_image(handle, &cat_l8_raw_image_properties, &cat_l8_raw_image_handle);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_image_manager_load_image failed with %u\n", res);
+        return res;
+    }
+
+    // then the argb1555 image
+    FT81X_Image_Handle cat_argb1555_raw_image_handle;
+    res = ft81x_image_manager_load_image(handle, &cat_argb1555_raw_image_properties, &cat_argb1555_raw_image_handle);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_image_manager_load_image failed with %u\n", res);
+        return res;
+    }
+
+    // then the paletted8 image
+    FT81X_Image_Handle cat_paletted8_raw_image_handle;
+    res = ft81x_image_manager_load_image(handle, &cat_paletted8_raw_image_properties, &cat_paletted8_raw_image_handle);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_image_manager_load_image failed with %u\n", res);
+        return res;
+    }
+
+    // ----------------------------------------------------------
+    // Build and display the display list
+    // ----------------------------------------------------------
+    // clear the screen
+    res = ft81x_write_display_list_snippet(handle, sizeof(clear_dl_snippet), clear_dl_snippet);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
+        return res;
+    }
+
+    // initialise the L8 image in the DL
+    res = ft81x_image_manager_send_image_init_dl(handle,
+                                                 &cat_l8_raw_image_properties,
+                                                 &cat_l8_raw_image_handle,
+                                                 FT81X_BITMAP_FILTER_NEAREST,
+                                                 FT81X_BITMAP_WRAP_BORDER,
+                                                 FT81X_BITMAP_WRAP_BORDER);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
+        return res;
+    }
+
+    // initialise the argb1555 image in the DL
+    res = ft81x_image_manager_send_image_init_dl(handle,
+                                                 &cat_argb1555_raw_image_properties,
+                                                 &cat_argb1555_raw_image_handle,
+                                                 FT81X_BITMAP_FILTER_NEAREST,
+                                                 FT81X_BITMAP_WRAP_BORDER,
+                                                 FT81X_BITMAP_WRAP_BORDER);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
+        return res;
+    }
+
+    // initialise the paletted8 image in the DL
+    res = ft81x_image_manager_send_image_init_dl(handle,
+                                                 &cat_paletted8_raw_image_properties,
+                                                 &cat_paletted8_raw_image_handle,
+                                                 FT81X_BITMAP_FILTER_NEAREST,
+                                                 FT81X_BITMAP_WRAP_BORDER,
+                                                 FT81X_BITMAP_WRAP_BORDER);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
+        return res;
+    }
+
+    // draw L8 image
+    res = ft81x_image_manager_send_non_paletted8_image_draw_dl(handle,
+                                                               &cat_l8_raw_image_handle,
+                                                               160-(cat_l8_raw_image_properties.width/2),
+                                                               240-(cat_l8_raw_image_properties.height/2));
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
+        return res;
+    }
+
+    // draw ARGB1555 image
+    res = ft81x_image_manager_send_non_paletted8_image_draw_dl(handle,
+                                                               &cat_argb1555_raw_image_handle,
+                                                               0, 0);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
+        return res;
+    }
+
+    // draw paletted8 image
+    // (note saves the context on the stack and restores it)
+    res = ft81x_image_manager_send_paletted8_image_draw_dl(handle,
+                                                           &cat_paletted8_raw_image_handle,
+                                                           162, 270);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
+        return res;
+    }
+
+    // draw some points, lines and rects
+    res = ft81x_write_display_list_snippet(handle, sizeof(test_dl_snippet), test_dl_snippet);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_snippet failed with %u\n", res);
+        return res;
+    }
+
+    // write some text strings
+    res = test1_text(handle);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("test1_text failed with %u\n", res);
+        return res;
+    }
+
+    // display it
+    res = ft81x_write_display_list_cmd(handle, FT81X_DL_CMD_DISPLAY());
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_write_display_list_cmd failed with %u\n", res);
+        return res;
+    }
+
+    // write everything to the DL ram and then swap it in
+    res = ft81x_end_display_list(handle);
+    if (res != FT81X_RESULT_OK)
+    {
+        DbgConsole_Printf("ft81x_end_display_list failed with %u\n", res);
+        return res;
+    }
+
+    return FT81X_RESULT_OK;
+}
