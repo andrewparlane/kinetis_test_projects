77fe6881426c4c77394ac29a7b0c924582635c0a
Andrew Parlane <andrew.parlane@gmail.com>
2016-09-24 18:03:24 -0400

    FT81X_LIB CoProcessor: Added support for CMD_TEXT.

--- common/libs/ft81x_lib/include/ft81x_co_processor.h
+++ common/libs/ft81x_lib/include/ft81x_co_processor.h
@@ -23,6 +23,18 @@ ft81x_result ft81x_coproc_cmd_dlstart(FT81X_Handle *handle);
 // Swaps the display list (uses REG_DLSWAP)
 ft81x_result ft81x_coproc_cmd_swap(FT81X_Handle *handle);
 
+// Draws font to the screen
+//  x    - x co-ord of text (see options)
+//  y    - y co-ord of text (see options)
+//  font - bitmap id for font 0-31 (0-14 are for custom RAM fonts)
+//                                    (15 is for scratch)
+//                                    (16-31 are ROM fonts)
+//                                    (there are also 32 - 34, see CMD_ROMFONT)
+//  options - how to align the text with the x,y co-ords
+//            see FT81X_COPROC_OPTION_CENTRE... and FT81X_COPROC_OPTION_RIGHTX
+//  str     - NULL terminated ascii string
+ft81x_result ft81x_coproc_cmd_text(FT81X_Handle *handle, uint16_t x, uint16_t y, uint8_t font_id, uint16_t options, char *str);
+
 // Show the FTDI logo animation
 ft81x_result ft81x_coproc_cmd_logo(FT81X_Handle *handle);
 
--- common/libs/ft81x_lib/src/co_processor.c
+++ common/libs/ft81x_lib/src/co_processor.c
@@ -32,7 +32,48 @@ inline ft81x_result ft81x_coproc_cmd_swap(FT81X_Handle *handle)
     return ft81x_graphics_engine_write_display_list_cmd(handle, FT81X_COPROC_CMD_ID_SWAP);
 }
 
-ft81x_result ft81x_coproc_cmd_logo(FT81X_Handle *handle)
+ft81x_result ft81x_coproc_cmd_text(FT81X_Handle *handle, uint16_t x, uint16_t y, uint8_t font_id, uint16_t options, char *str)
+{
+    // first the command and the parameters
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_TEXT,
+                              (y << 16) | x,
+                              (options << 16) | font_id };
+
+    ft81x_result res = ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+    if (res != FT81X_RESULT_OK)
+    {
+        return res;
+    }
+
+    // then the string
+    // (strlen + 1 for the NULL char
+    uint32_t len = strlen(str) + 1;
+
+    // this needs to be padded to 4 bytes.
+    // so I send all blocks of 4 and then
+    // finally any remaining bytes padded out
+    // hence the len & ~3
+    res = ft81x_graphics_engine_write_display_list_snippet(handle, len & ~3, (uint32_t *)str);
+    if (res != FT81X_RESULT_OK)
+    {
+        return res;
+    }
+
+    // anything remaining?
+    if (len % 4)
+    {
+        // copy remaining data (1,2 or 3 bytes)
+        uint8_t remaining_data[4];
+        memcpy(remaining_data, &str[len & ~3], len % 4);
+
+        // doesn't matter on the pad values, so just send it
+        return ft81x_graphics_engine_write_display_list_snippet(handle, 4, (uint32_t *)remaining_data);
+    }
+
+    return FT81X_RESULT_OK;
+}
+
+inline ft81x_result ft81x_coproc_cmd_logo(FT81X_Handle *handle)
 {
     return ft81x_graphics_engine_write_display_list_cmd(handle, FT81X_COPROC_CMD_ID_LOGO);
 }
