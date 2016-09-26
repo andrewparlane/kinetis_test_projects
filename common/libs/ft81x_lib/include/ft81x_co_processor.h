1eb5c9ac8109ba18cf936138219126d474f038ec
Andrew Parlane <andrew.parlane@gmail.com>
2016-09-25 21:55:20 -0400

    FT81X_LIB CoProcessor: Added a bunch more commands:
    CMD_BUTTON
    CMD_CLOCK
    CMD_BGCOLOUR
    CMD_FGCOLOUR
    CMD_GRADCOLOUR
    CMD_GAUGE
    CMD_GRADIENT
    CMD_KEYS
    CMD_PROGRESS
    CMD_SCROLLBAR
    CMD_SLIDER
    CMD_DIAL
    CMD_TOGGLE
    CMD_NUMBER
    CMD_COLDSTART

--- common/libs/ft81x_lib/include/ft81x_co_processor.h
+++ common/libs/ft81x_lib/include/ft81x_co_processor.h
@@ -8,16 +8,40 @@
 // ----------------------------------------------------------------------------
 // Option flags
 // ----------------------------------------------------------------------------
+#define FT81X_COPROC_OPTION_FLAT            0x0100
+#define FT81X_COPROC_OPTION_SIGNED          0x0100
 #define FT81X_COPROC_OPTION_CENTREX         0x0200
 #define FT81X_COPROC_OPTION_CENTREY         0x0400
 #define FT81X_COPROC_OPTION_CENTRE          ((FT81X_COPROC_OPTION_CENTREX) | (FT81X_COPROC_OPTION_CENTREY))
 #define FT81X_COPROC_OPTION_RIGHTX          0x0800
+#define FT81X_COPROC_OPTION_NOBACK          0x1000
+#define FT81X_COPROC_OPTION_NOTICKS         0x2000
+#define FT81X_COPROC_OPTION_NOHM            0x4000
+#define FT81X_COPROC_OPTION_NOPOINTER       0x4000
+#define FT81X_COPROC_OPTION_NOSECS          0x8000
+#define FT81X_COPROC_OPTION_NOHANDS         ((FT81X_COPROC_OPTION_NOSECS) | (FT81X_COPROC_OPTION_NOHM))
 
 // Starts a new display list
 ft81x_result ft81x_coproc_cmd_dlstart(FT81X_Handle *handle);
 
 // Swaps the display list (uses REG_DLSWAP)
 ft81x_result ft81x_coproc_cmd_swap(FT81X_Handle *handle);
+
+// Sets the background colour of widgets
+ft81x_result ft81x_coproc_cmd_bgcolour(FT81X_Handle *handle, uint8_t r, uint8_t g, uint8_t b);
+
+// Sets the foreground colour of widgets
+ft81x_result ft81x_coproc_cmd_fgcolour(FT81X_Handle *handle, uint8_t r, uint8_t g, uint8_t b);
+
+// Draws a gradient on the screen
+// Should be used with CMD_SCISSOR_XY and CMD_SCISSOR_SIZE for the desired effect
+// interpolates a gradient between fictional points x0,y0 and x1,y1
+// and the colours r0,g0,b0, and r1,g1,b1
+// if you want a horizontal gradient set y0 = y1.
+//             a vertical gradient set x0 = x1.
+//             a mostly horizontal but slight vertical gradient
+//               set x0 = 0, y0 = 0, x1 = 300, y1 = 10
+ft81x_result ft81x_coproc_cmd_gradient(FT81X_Handle *handle, uint16_t x0, uint16_t y0, uint8_t r0, uint8_t g0, uint8_t b0, uint16_t x1, uint16_t y1, uint8_t r1, uint8_t g1, uint8_t b1);
 
 // Draws font to the screen
 //  x    - x co-ord of text (see options)
@@ -31,6 +55,113 @@ ft81x_result ft81x_coproc_cmd_swap(FT81X_Handle *handle);
 //  str     - NULL terminated ascii string
 ft81x_result ft81x_coproc_cmd_text(FT81X_Handle *handle, uint16_t x, uint16_t y, uint8_t font_id, uint16_t options, char *str);
 
+// Draws a button to the screen
+//  x      - x co-ord of button
+//  y      - y co-ord of button
+//  width  - width of button
+//  height - height of button
+//  font   - bitmap id for font 0-31 (0-14 are for custom RAM fonts)
+//                                   (15 is for scratch)
+//                                   (16-31 are ROM fonts)
+//                                   (there are also 32 - 34, see CMD_ROMFONT)
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//  str     - button label (NULL terminated ascii string)
+ft81x_result ft81x_coproc_cmd_button(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t font_id, uint16_t options, char *str);
+
+// Draws a set of keys horizontally access the screen
+//  x      - x co-ord of top left of keys
+//  y      - y co-ord of top left of keys
+//  width  - width of keys (in total, not each)
+//  height - height of keys
+//  font   - bitmap id for font 0-31 (0-14 are for custom RAM fonts)
+//                                   (15 is for scratch)
+//                                   (16-31 are ROM fonts)
+//                                   (there are also 32 - 34, see CMD_ROMFONT)
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//            FT81X_COPROC_OPTION_CENTRE keys are drawn at the minimum size
+//                                       (based on font) in the centre of
+//                                       the x,y, x+width, y+height rectangle
+//            ascii code if an ascii code is specified, that key is shown as pressed.
+//            IE. FT81X_COPROC_OPTION_FLAT | 'a' draws the keys flat and shows 'a' as pressed
+//  str     - key labels (one per key) (NULL terminated ascii string)
+ft81x_result ft81x_coproc_cmd_keys(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t font_id, uint16_t options, char *str);
+
+// Draws a progress bar to the screen
+//  x       - top left co-ord
+//  y       - top left co-ord
+//  width   - width in pixels
+//  height  - height in pixels
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//  val     - value to show between 0 - range
+//  range   - maximum value
+ft81x_result ft81x_coproc_cmd_progress(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t options, uint16_t val, uint16_t range);
+
+// Draws a slider bar to the screen
+//  x       - top left co-ord
+//  y       - top left co-ord
+//  width   - width in pixels
+//  height  - height in pixels
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//  val     - value to show between 0 - range
+//  range   - maximum value
+ft81x_result ft81x_coproc_cmd_slider(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t options, uint16_t val, uint16_t range);
+
+// Draws a scrollbar to the screen
+//  x       - top left co-ord
+//  y       - top left co-ord
+//  width   - width in pixels
+//  height  - height in pixels
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//  val     - value to show between 0 - range
+//  size    - how far the bar extends
+//  range   - maximum value
+ft81x_result ft81x_coproc_cmd_scrollbar(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t options, uint16_t val, uint16_t size, uint16_t range);
+
+// Draws a toggle to the screen
+//  y      - y co-ord of top left of toggle
+//  x      - x co-ord of top left of toggle
+//  width  - width of toggle
+//  font   - bitmap id for font 0-31 (0-14 are for custom RAM fonts)
+//                                   (15 is for scratch)
+//                                   (16-31 are ROM fonts)
+//                                   (there are also 32 - 34, see CMD_ROMFONT)
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//  state   - 0 = off, anything else = on
+//  str     - labels (NULL terminated ascii string)
+//            the two state labels are seperated with '\xff'
+//            e.g "off\xffon"
+ft81x_result ft81x_coproc_cmd_toggle(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint8_t font_id, uint16_t options, uint16_t state, char *str);
+
+// Draws a gauge to the screen
+//  x       - x co-ord of gauge
+//  y       - y co-ord of gauge
+//  radius  - radius of the gauge
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//            FT81X_COPROC_OPTION_NOBACK for no background
+//            FT81X_COPROC_OPTION_NOTICKS for no tick marks
+//            FT81X_COPROC_OPTION_NOPOINTER for no hand
+//  major   - number of major subdivisions on the gauge (1-10)
+//  minor   - number of minor subdivisions per major tick  (1-10)
+//  val     - value indicated between 0 - range
+//  range   - maximum value
+ft81x_result ft81x_coproc_cmd_gauge(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t radius, uint16_t options, uint16_t major, uint16_t minor, uint16_t val, uint16_t range);
+
+// Draw a clock to the screen
+//  x       - x co-ord of clock
+//  y       - y co-ord of clock
+//  radius  - radius of the clock
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//            FT81X_COPROC_OPTION_NOBACK for no background
+//            FT81X_COPROC_OPTION_NOTICKS for no tick marks
+//            FT81X_COPROC_OPTION_NOHM for no hour or minute hands
+//            FT81X_COPROC_OPTION_NOSECS for no second hand
+//            FT81X_COPROC_OPTION_NOHANDS for no hands
+//  hours   - time in hours (0 - 12)
+//  minutes - time in minutes (0 - 59)
+//  seconds - time in seconds (0 - 59)
+//  milliseconds - time in ms (0 - 1000)
+ft81x_result ft81x_coproc_cmd_clock(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t radius, uint16_t options, uint16_t hours, uint16_t minutes, uint16_t seconds, uint16_t milliseconds);
+
 // Decompresses data that has been compressed with the DEFLATE algorithm
 // for example using the zlib library.
 // Note: This should be used in conjunction with the G_RAM manager.
@@ -43,7 +174,37 @@ ft81x_result ft81x_coproc_cmd_inflate(FT81X_Handle *handle, uint32_t offset, uin
 // Get the end memory address of data inflated using CMD_INFLATE
 ft81x_result ft81x_coproc_cmd_getptr(FT81X_Handle *handle);
 
+// Draw a rotary dial to the screen
+//  x       - x co-ord of clock
+//  y       - y co-ord of clock
+//  radius  - radius of the clock
+//  options - FT81X_COPROC_OPTION_FLAT for no 3D effect
+//  val     - 0 = down, 0x4000 = left, 0x8000 = up, 0xC000 = right
+ft81x_result ft81x_coproc_cmd_dial(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t radius, uint16_t options, uint16_t val);
+
+// Draw a number to the screen
+//  x       - x co-ord of number
+//  y       - y co-ord of number
+//  font   - bitmap id for font 0-31 (0-14 are for custom RAM fonts)
+//                                   (15 is for scratch)
+//                                   (16-31 are ROM fonts)
+//                                   (there are also 32 - 34, see CMD_ROMFONT)
+//  options - FT81X_COPROC_OPTION_SIGNED for signed number
+//            FT81X_COPROC_OPTION_CENTREX
+//            FT81X_COPROC_OPTION_CENTREY
+//            FT81X_COPROC_OPTION_CENTRE
+//            FT81X_COPROC_OPTION_RIGHTX
+//  num     - the number to display 32 bit, signed or unsigned (depending on options
+ft81x_result ft81x_coproc_cmd_number(FT81X_Handle *handle, uint16_t x, uint16_t y, uint8_t font_id, uint16_t options, int32_t num);
+
 // Show the FTDI logo animation
 ft81x_result ft81x_coproc_cmd_logo(FT81X_Handle *handle);
 
+// Sets the co-proc to default reset states
+ft81x_result ft81x_coproc_cmd_coldstart(FT81X_Handle *handle);
+
+// Change the gradient colour for buttons and keys
+// A button or key starts with the gradient colour at the top
+// and fades to the fg colour at the bottom
+ft81x_result ft81x_coproc_cmd_gradcolour(FT81X_Handle *handle, uint8_t r, uint8_t g, uint8_t b);
 #endif
--- common/libs/ft81x_lib/src/co_processor.c
+++ common/libs/ft81x_lib/src/co_processor.c
@@ -11,10 +11,25 @@
 // ----------------------------------------------------------------------------
 #define FT81X_COPROC_CMD_ID_DLSTART         0xFFFFFF00
 #define FT81X_COPROC_CMD_ID_SWAP            0xFFFFFF01
+#define FT81X_COPROC_CMD_ID_BGCOLOUR        0xFFFFFF09
+#define FT81X_COPROC_CMD_ID_FGCOLOUR        0xFFFFFF0A
+#define FT81X_COPROC_CMD_ID_GRADIENT        0xFFFFFF0B
 #define FT81X_COPROC_CMD_ID_TEXT            0xFFFFFF0C
+#define FT81X_COPROC_CMD_ID_BUTTON          0xFFFFFF0D
+#define FT81X_COPROC_CMD_ID_KEYS            0xFFFFFF0E
+#define FT81X_COPROC_CMD_ID_PROGRESS        0xFFFFFF0F
+#define FT81X_COPROC_CMD_ID_SLIDER          0xFFFFFF10
+#define FT81X_COPROC_CMD_ID_SCROLLBAR       0xFFFFFF11
+#define FT81X_COPROC_CMD_ID_TOGGLE          0xFFFFFF12
+#define FT81X_COPROC_CMD_ID_GAUGE           0xFFFFFF13
+#define FT81X_COPROC_CMD_ID_CLOCK           0xFFFFFF14
 #define FT81X_COPROC_CMD_ID_INFLATE         0xFFFFFF22
 #define FT81X_COPROC_CMD_ID_GETPTR          0xFFFFFF23
+#define FT81X_COPROC_CMD_ID_DIAL            0xFFFFFF2D
+#define FT81X_COPROC_CMD_ID_NUMBER          0xFFFFFF2E
 #define FT81X_COPROC_CMD_ID_LOGO            0xFFFFFF31
+#define FT81X_COPROC_CMD_ID_COLDSTART       0xFFFFFF32
+#define FT81X_COPROC_CMD_ID_GRADCOLOUR      0xFFFFFF34
 
 // ----------------------------------------------------------------------------
 // CMD functions
@@ -28,6 +43,33 @@ inline ft81x_result ft81x_coproc_cmd_dlstart(FT81X_Handle *handle)
 inline ft81x_result ft81x_coproc_cmd_swap(FT81X_Handle *handle)
 {
     return ft81x_graphics_engine_write_display_list_cmd(handle, FT81X_COPROC_CMD_ID_SWAP);
+}
+
+ft81x_result ft81x_coproc_cmd_bgcolour(FT81X_Handle *handle, uint8_t r, uint8_t g, uint8_t b)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_BGCOLOUR,
+                              (r << 16) | (g << 8) | b };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+}
+
+ft81x_result ft81x_coproc_cmd_fgcolour(FT81X_Handle *handle, uint8_t r, uint8_t g, uint8_t b)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_FGCOLOUR,
+                              (r << 16) | (g << 8) | b };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+}
+
+ft81x_result ft81x_coproc_cmd_gradient(FT81X_Handle *handle, uint16_t x0, uint16_t y0, uint8_t r0, uint8_t g0, uint8_t b0, uint16_t x1, uint16_t y1, uint8_t r1, uint8_t g1, uint8_t b1)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_GRADIENT,
+                              (y0 << 16) | x0,
+                              (r0 << 16) | (g0 << 8) | b0,
+                              (y1 << 16) | x1,
+                              (r1 << 16) | (g1 << 8) | b1 };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
 }
 
 ft81x_result ft81x_coproc_cmd_text(FT81X_Handle *handle, uint16_t x, uint16_t y, uint8_t font_id, uint16_t options, char *str)
@@ -47,6 +89,128 @@ ft81x_result ft81x_coproc_cmd_text(FT81X_Handle *handle, uint16_t x, uint16_t y,
     // (strlen + 1 for the NULL char
     uint32_t len = strlen(str) + 1;
     return ft81x_graphics_engine_write_display_list_data(handle, len, (uint8_t *)str);
+}
+
+ft81x_result ft81x_coproc_cmd_button(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t font_id, uint16_t options, char *str)
+{
+    // first the command and the parameters
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_BUTTON,
+                              (y << 16) | x,
+                              (height << 16) | width,
+                              (options << 16) | font_id };
+
+    ft81x_result res = ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+    if (res != FT81X_RESULT_OK)
+    {
+        return res;
+    }
+
+    // then the string
+    // (strlen + 1 for the NULL char
+    uint32_t len = strlen(str) + 1;
+    return ft81x_graphics_engine_write_display_list_data(handle, len, (uint8_t *)str);
+}
+
+ft81x_result ft81x_coproc_cmd_keys(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint8_t font_id, uint16_t options, char *str)
+{
+    // first the command and the parameters
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_KEYS,
+                              (y << 16) | x,
+                              (height << 16) | width,
+                              (options << 16) | font_id };
+
+    ft81x_result res = ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+    if (res != FT81X_RESULT_OK)
+    {
+        return res;
+    }
+
+    // then the string
+    // (strlen + 1 for the NULL char
+    uint32_t len = strlen(str) + 1;
+    return ft81x_graphics_engine_write_display_list_data(handle, len, (uint8_t *)str);
+}
+
+ft81x_result ft81x_coproc_cmd_progress(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t options, uint16_t val, uint16_t range)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_PROGRESS,
+                              (y << 16) | x,
+                              (height << 16) | width,
+                              (val << 16) | options,
+                              range };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+}
+
+ft81x_result ft81x_coproc_cmd_slider(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t options, uint16_t val, uint16_t range)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_SLIDER,
+                              (y << 16) | x,
+                              (height << 16) | width,
+                              (val << 16) | options,
+                              range };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+}
+
+ft81x_result ft81x_coproc_cmd_scrollbar(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t options, uint16_t val, uint16_t size, uint16_t range)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_SCROLLBAR,
+                              (y << 16) | x,
+                              (height << 16) | width,
+                              (val << 16) | options,
+                              (range << 16) | size};
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+}
+
+ft81x_result ft81x_coproc_cmd_toggle(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t width, uint8_t font_id, uint16_t options, uint16_t state, char *str)
+{
+    // state 0 is off, 65535 is on
+    if (state)
+    {
+        state = 65535;
+    }
+
+    // first the command and the parameters
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_TOGGLE,
+                              (y << 16) | x,
+                              (font_id << 16) | width,
+                              (state << 16) | options };
+
+    ft81x_result res = ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+    if (res != FT81X_RESULT_OK)
+    {
+        return res;
+    }
+
+    // then the string
+    // (strlen + 1 for the NULL char
+    uint32_t len = strlen(str) + 1;
+    return ft81x_graphics_engine_write_display_list_data(handle, len, (uint8_t *)str);
+}
+
+
+ft81x_result ft81x_coproc_cmd_gauge(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t radius, uint16_t options, uint16_t major, uint16_t minor, uint16_t val, uint16_t range)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_GAUGE,
+                              (y << 16) | x,
+                              (options << 16) | radius,
+                              (minor << 16) | major,
+                              (range << 16) | val };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+}
+
+ft81x_result ft81x_coproc_cmd_clock(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t radius, uint16_t options, uint16_t hours, uint16_t minutes, uint16_t seconds, uint16_t milliseconds)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_CLOCK,
+                              (y << 16) | x,
+                              (options << 16) | radius,
+                              (minutes << 16) | hours,
+                              (milliseconds << 16) | seconds };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
 }
 
 ft81x_result ft81x_coproc_cmd_inflate(FT81X_Handle *handle, uint32_t offset, uint32_t count, const uint8_t *compressed_data)
@@ -74,7 +238,41 @@ ft81x_result ft81x_coproc_cmd_getptr(FT81X_Handle *handle)
     return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(cmd), cmd);
 }
 
+ft81x_result ft81x_coproc_cmd_dial(FT81X_Handle *handle, uint16_t x, uint16_t y, uint16_t radius, uint16_t options, uint16_t val)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_DIAL,
+                              (y << 16) | x,
+                              (options << 16) | radius,
+                              val };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+}
+
+ft81x_result ft81x_coproc_cmd_number(FT81X_Handle *handle, uint16_t x, uint16_t y, uint8_t font_id, uint16_t options, int32_t num)
+{
+    // first the command and the parameters
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_NUMBER,
+                              (y << 16) | x,
+                              (options << 16) | font_id,
+                              num };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
+}
+
 inline ft81x_result ft81x_coproc_cmd_logo(FT81X_Handle *handle)
 {
     return ft81x_graphics_engine_write_display_list_cmd(handle, FT81X_COPROC_CMD_ID_LOGO);
+}
+
+inline ft81x_result ft81x_coproc_cmd_coldstart(FT81X_Handle *handle)
+{
+    return ft81x_graphics_engine_write_display_list_cmd(handle, FT81X_COPROC_CMD_ID_COLDSTART);
+}
+
+ft81x_result ft81x_coproc_cmd_gradcolour(FT81X_Handle *handle, uint8_t r, uint8_t g, uint8_t b)
+{
+    const uint32_t data[] = { FT81X_COPROC_CMD_ID_GRADCOLOUR,
+                              (r << 16) | (g << 8) | b };
+
+    return ft81x_graphics_engine_write_display_list_snippet(handle, sizeof(data), data);
 }
