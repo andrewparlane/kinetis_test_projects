caa4ba933cfc98c2c6219bb0bb20897fc1964855
Andrew Parlane <andrew.parlane@gmail.com>
2016-10-01 18:49:39 -0400

    screen_test: Removed the text output code from test1.
    This is because the old monospaced font output function was removed from
    the FT81X library.

--- frdm_k66f_board/screen_test/tests/test1.c
+++ frdm_k66f_board/screen_test/tests/test1.c
@@ -59,48 +59,6 @@ static const uint32_t test_dl_snippet[] =
         FT81X_DL_CMD_END(),
 };
 
-static ft81x_result test1_text(FT81X_Handle *handle)
-{
-    ft81x_result res;
-
-    // set the font to green
-    res = ft81x_graphics_engine_write_display_list_cmd(handle, FT81X_DL_CMD_COLOUR_RGB(0, 255, 0));
-    if (res != FT81X_RESULT_OK)
-    {
-        return res;
-    }
-
-    res = ft81x_text_manager_write_fixed_width_font_string(handle,
-                                                           24,
-                                                           FT81X_TEXT_MANAGER_FONT_24_MAX_WIDTH,
-                                                           FT81X_TEXT_MANAGER_FONT_24_HEIGHT,
-                                                           "How much wood\nwould a wood chuck\nchuck, if a wood\nchuck could\nchuck wood?",
-                                                           10,
-                                                           40);
-    if (res != FT81X_RESULT_OK)
-    {
-        return res;
-    }
-
-    // set the font to cyan
-    res = ft81x_graphics_engine_write_display_list_cmd(handle, FT81X_DL_CMD_COLOUR_RGB(0, 255, 255));
-    if (res != FT81X_RESULT_OK)
-    {
-        return res;
-    }
-
-    res = ft81x_text_manager_write_fixed_width_font_string(handle,
-                                                           30,
-                                                           FT81X_TEXT_MANAGER_FONT_30_MAX_WIDTH,
-                                                           FT81X_TEXT_MANAGER_FONT_30_HEIGHT,
-                                                           "Peter\nPiper\npicked...",
-                                                           10,
-                                                           300);
-
-
-    return res;
-}
-
 ft81x_result test1(FT81X_Handle *handle)
 {
     ft81x_result res;
@@ -229,14 +187,6 @@ ft81x_result test1(FT81X_Handle *handle)
     if (res != FT81X_RESULT_OK)
     {
         DbgConsole_Printf("ft81x_graphics_engine_write_display_list_snippet failed with %u\n", res);
-        return res;
-    }
-
-    // write some text strings
-    res = test1_text(handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("test1_text failed with %u\n", res);
         return res;
     }
 
