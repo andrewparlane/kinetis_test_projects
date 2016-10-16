010a8eddd658fcebbfe02d5fbbd040570b4adf32
Andrew Parlane <andrew.parlane@gmail.com>
2016-10-16 16:44:05 -0400

    screen_test: Moved the ft81x_lib setup and the running of tests to a new common file.

--- frdm_k66f_board/screen_test/main.c
+++ frdm_k66f_board/screen_test/main.c
@@ -44,9 +44,6 @@
 #include "fsl_dmamux.h"
 #include "fsl_edma.h"
 
-// ft81x lib includes
-#include "ft81x.h"
-
 // ----------------------------------------------------------------------------
 // GPIO pins
 // ----------------------------------------------------------------------------
@@ -65,18 +62,10 @@ const GPIO_Info *pin_config[] = { &screen_int_pin };
 #define NUM_GPIO_PINS (sizeof(pin_config) / sizeof(pin_config[0]))
 
 // ----------------------------------------------------------------------------
-// FT81X stuff
-// ----------------------------------------------------------------------------
-#define DL_BUFFER_SIZE 512
-uint8_t dl_buffer[DL_BUFFER_SIZE];
-
-// ----------------------------------------------------------------------------
 // Test forward defines
 // ----------------------------------------------------------------------------
 
-ft81x_result test1(FT81X_Handle *handle);
-ft81x_result test2(FT81X_Handle *handle);
-ft81x_result test3(FT81X_Handle *handle);
+void init_lib_and_run_tests();
 
 // ----------------------------------------------------------------------------
 // Main thread
@@ -111,69 +100,7 @@ static void main_thread(void *arg)
     EDMA_GetDefaultConfig(&edma_config);
     EDMA_Init(DMA0, &edma_config);
 
-    // set up the FT81X lib
-    // this initialisers the spi module and the dma channels
-    FT81X_Handle handle;
-    memset(&handle, 0, sizeof(FT81X_Handle));
-    ft81x_result res = ft81x_initialise(&handle, DL_BUFFER_SIZE, dl_buffer);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_initialise failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-    res = ft81x_configure(&handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_configure failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // ----------------------------------------------------------
-    // enable the backlight
-    // you can do this after displaying the first screen
-    // to avoid the black screen)
-    // ----------------------------------------------------------
-    res = ft81x_backlight(&handle, FT81X_BACKLIGHT_LEVEL_MAX);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("ft81x_backlight failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // draws some images, and primitives
-    // all directly through the DL
-    res = test1(&handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("test1 failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // tests using the co-proc
-    res = test2(&handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("test2 failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    // touch stuff
-    res = test3(&handle);
-    if (res != FT81X_RESULT_OK)
-    {
-        DbgConsole_Printf("test3 failed with %u\n", res);
-        ft81x_cleanup(&handle);
-        return;
-    }
-
-    DbgConsole_Printf("sreen_test completed successfully\n");
-
-    ft81x_cleanup(&handle);
+    init_lib_and_run_tests();
 }
 
 // ----------------------------------------------------------------------------
